ed6b56f0b635fbc239bbc3016d0049f33111e139
laludonisetiawan <laludonisetiawan@gmail.com>
2023-03-21 00:47:15 +0800

    commit pertama

---
+++ resources/views/header/headers.blade.php
@@ -0,0 +1,39 @@
+<!-- header start -->
+<header class="bg-transparent absolute top-0 left-0 w-full flex items-center z-10">
+    <div class="container">
+        <div class="flex items-center justify-between relative">
+            <div class="px-4">
+                <a href="#home" class="font-bold text-lg text-primary block py-6">laludoni</a>
+            </div>
+        <div class="flex items-center px-4">
+            <button id="hamburger" name="hamburger" type="button" class="block absolute right-4 lg:hidden" >
+                <span class="w-[30px] h-[2px] my-2 block bg-dark"></span>
+                <span class="w-[30px] h-[2px] my-2 block bg-dark"></span>
+                <span class="w-[30px] h-[2px] my-2 block bg-dark"></span>
+            </button>
+
+            <nav id="nav-menu" class="hidden absolute py-5 bg-white shadow-lg rounded-lg max-w-[250px] w-full right-4 top-full lg:block lg:static lg:bg-transparent lg:max-w-full lg:shadow-none lg:rounded-none">
+                <ul class="block lg:flex">
+                    <li class="group">
+                        <a href="#home" class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">beranda</a>
+                    </li>
+                    <li class="group">
+                        <a href="#about" class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Tentang Saya</a>
+                    </li>
+                    <li class="group">
+                        <a href="#portofolio" class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Portofolio</a>
+                    </li>
+                    <li class="group">
+                        <a href="#blog" class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">blog</a>
+                    </li>
+                    <li class="group">
+                        <a href="#contact" class="text-base text-dark py-2 mx-8 flex group-hover:text-primary">Contact</a>
+                    </li>
+                </ul>
+            </nav>
+        </div>
+        </div>
+    </div>
+
+</header>
+<!-- header end -->
