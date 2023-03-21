29a4a934a015aa4446baf59755acaf6617843ba9
laludonisetiawan <laludonisetiawan@gmail.com>
2023-03-21 14:43:09 +0800

    finishing

---
+++ resources/views/template/partials/form.blade.php
@@ -0,0 +1,54 @@
+<section id="kontak"  class="pt-36">
+    <div class="container mx-auto mt-8">
+        <div class="max-w-lg mx-auto bg-white rounded-lg shadow-lg p-6">
+          <h2 class="text-2xl font-semibold mb-4">Hubungi Kami</h2>
+          <form>
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="name">
+                Nama
+              </label>
+              <input
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-500"
+                id="name"
+                type="text"
+                name="name"
+                placeholder="Masukkan Nama Anda"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="email">
+                Email
+              </label>
+              <input
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-500"
+                id="email"
+                type="email"
+                name="email"
+                placeholder="Masukkan Email Anda"
+              />
+            </div>
+            <div class="mb-4">
+              <label class="block text-gray-700 font-bold mb-2" for="message">
+                Pesan
+              </label>
+              <textarea
+                class="w-full px-3 py-2 border rounded-lg focus:outline-none focus:border-teal-500"
+                id="message"
+                name="message"
+                rows="4"
+                placeholder="Tulis pesan Anda di sini"
+              ></textarea>
+            </div>
+            <div class="mt-6">
+              <button
+                class="w-full bg-teal-700 hover:bg-teal-800 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+                type="submit"
+              >
+                Kirim
+              </button>
+            </div>
+          </form>
+        </div>
+      </div>
+      
+</section>
---
+++ resources/views/template/partials/karya.blade.php
@@ -0,0 +1,11 @@
+<section id="portofolio" class="pt-2 pb-16 bg-slate-100">
+    <div class="container">
+        <div class="w-full px-4">
+            <div class="max-w-xl mx-auto text-center ">
+                <h3 class="font-semibold text-teal-700 text-2xl mb-4 lg:text-3xl lg:pt-10">Karya</h3>
+                <img src="{{('img/karya.png')}}" alt="">
+                
+            </div>
+        </div>
+    </div>
+</section>
